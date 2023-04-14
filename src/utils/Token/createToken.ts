c84d1126ffa88611c62e065e4eb46e1fd02d2861
Ahmed Mamdouh <tatah.douh.vom@gmail.com>
2023-04-14 15:18:18 +0200

    updating the createToken function to make it more reusable

---
+++ src/utils/Token/createToken.ts
@@ -0,0 +1,47 @@
+import { sign } from "jsonwebtoken";
+import UnhandledError from "../../Errors/UnhandledError";
+
+interface TokenObject {
+	accessToken: string;
+	refreshToken: string;
+}
+
+interface Options {
+	data: {
+		[key: string]: any;
+	};
+	accessSecret: string | undefined;
+	refreshSecret: string | undefined;
+	accessTokenExp?: string | number | undefined;
+	refreshTokenExp?: string | number | undefined;
+}
+
+// there is a better way to improve this by adding an options parameter
+export const createTokens = ({
+	data: { email, name },
+	refreshSecret,
+	accessSecret,
+	accessTokenExp = 60 * 10,
+	refreshTokenExp = "1 days",
+}: Options): TokenObject => {
+	if (!accessSecret) {
+		throw new UnhandledError(
+			"please provide an accessSecret secret to the createtoken function"
+		);
+	}
+	if (!refreshSecret) {
+		throw new UnhandledError(
+			"please provide an refreshSecret secret to the createtoken function"
+		);
+	}
+	const accessToken = sign({ name: name, email: email }, accessSecret, {
+		expiresIn: accessTokenExp,
+		noTimestamp: false,
+	});
+	const refreshToken = sign({ name: name, email: email }, refreshSecret, {
+		expiresIn: refreshTokenExp,
+		noTimestamp: false,
+	});
+
+	return { refreshToken, accessToken };
+};
