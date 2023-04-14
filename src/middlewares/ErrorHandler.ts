5a35e144368d2021eaad19110fef0a448478e744
Ahmed Mamdouh <tatah.douh.vom@gmail.com>
2023-04-14 15:16:10 +0200

    throwing an error when there is a data required missing

--- src/middlewares/ErrorHandler.ts
+++ src/middlewares/ErrorHandler.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import HttpError from "../Errors/HTTPError"
+import UnhandledError from "../Errors/UnhandledError";
 
 interface DublicationError {
   message: string;
@@ -25,6 +26,10 @@ export const errorHandler = (err:any,req:Request,res:Response,next:NextFunction)
     const message =`there is a doc with this ${Object.keys(duplicateError.keyValue)[0]}: ${Object.values(duplicateError.keyValue)[0]}`
     res.status(400).json({ message: message, dublicatKey: true })
     return 
+  }
+
+  if (err instanceof UnhandledError) {
+    throw err
   }
   res.status(500).json ({message:err.message})
 }
