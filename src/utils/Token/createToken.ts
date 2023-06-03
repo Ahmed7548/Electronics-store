1b365e6efd71fd2b12c9d544fb70434fb52be5db
Ahmed7548 <tatah.douh.vom@gmail.com>
2023-06-03 19:21:55 +0300

    up to date 2

--- src/utils/Token/createToken.ts
+++ src/utils/Token/createToken.ts
@@ -1,54 +1,3 @@
-<<<<<<< HEAD
-import * as jwt from "jsonwebtoken";
-import UnhandledError from "../../Errors/UnhandledError.js";
-
-const {sign}=jwt
-
-interface TokenObject {
-	accessToken: string;
-	refreshToken: string;
-}
-
-interface Options {
-	data: {
-		[key: string]: any;
-	};
-	accessSecret: string | undefined;
-	refreshSecret: string | undefined;
-	accessTokenExp?: string | number | undefined;
-	refreshTokenExp?: string | number | undefined;
-}
-
-// there is a better way to improve this by adding an options parameter
-export const createTokens = ({
-	data: { email, name },
-	refreshSecret,
-	accessSecret,
-	accessTokenExp = 60 * 10,
-	refreshTokenExp = "1 days",
-}: Options): TokenObject => {
-	if (!accessSecret) {
-		throw new UnhandledError(
-			"please provide an accessSecret secret to the createtoken function"
-		);
-	}
-	if (!refreshSecret) {
-		throw new UnhandledError(
-			"please provide an refreshSecret secret to the createtoken function"
-		);
-	}
-	const accessToken = sign({ name: name, email: email }, accessSecret, {
-		expiresIn: accessTokenExp,
-		noTimestamp: false,
-	});
-	const refreshToken = sign({ name: name, email: email }, refreshSecret, {
-		expiresIn: refreshTokenExp,
-		noTimestamp: false,
-	});
-
-	return { refreshToken, accessToken };
-};
-=======
 import * as jwt from "jsonwebtoken";
 import UnhandledError from "../../Errors/UnhandledError.js";
 
@@ -98,4 +47,3 @@ export const createTokens = ({
 
 	return { refreshToken, accessToken };
 };
->>>>>>> d14b9018711a8b32d57606d1a2439e7473237d2d
