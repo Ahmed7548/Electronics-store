fdc1eeb526c3e37a8d944c6b28513adf22b5bb26
Ahmed7548 <tatah.douh.vom@gmail.com>
2023-06-23 01:08:45 +0300

    refactore the initial data controller

--- src/controllers/initialData/initialData.ts
+++ src/controllers/initialData/initialData.ts
@@ -16,8 +16,8 @@ export const getInitialData:AsyncCustomRequestHandler<any,{userId:string|undefin
     const userId=req.user?.id
     const promotedProducts= Product.getPromoted(10)
     const categories= Category.find()
-    if(userId){
-        const user = User.findById(userId,{verificationToken:0,verified:0,googleId:0,password:0})
+    const user = User.findById(userId,{verificationToken:0,verified:0,googleId:0,password:0})
+   
         // get the categories and user specific data and promoted products
 
         const data=await  Promise.all([await user,await promotedProducts,await categories])
@@ -27,14 +27,6 @@ export const getInitialData:AsyncCustomRequestHandler<any,{userId:string|undefin
             promotedProducts:data[1],
             categories:data[2]
         })
-    }else {
-        const data=await  Promise.all([await promotedProducts,await categories])
-        res.status(200).json({
-            promotedProducts:data[0],
-            categories:data[1]
-        })
-        //get the categories and promoted products
-    }
 }
 
 
